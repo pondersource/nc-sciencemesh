e52c68188830c27343f635763103b74e6895b26e
Ismoil Shifoev <alex.malikov94@gmail.com>
2021-11-24 11:12:42 +0500

    Update a new class for generate a new token

---
+++ lib/TokenHandler/SciencemeshToken.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OCA\ScienceMesh\TokenHandler;
+
+
+use OCP\Security\ISecureRandom;
+
+class SciencemeshToken {
+    public const TOKEN_LENGTH = 15;
+
+	/** @var ISecureRandom */
+	private $secureRandom;
+
+	/**
+	 * TokenHandler constructor.
+	 *
+	 * @param ISecureRandom $secureRandom
+	 */
+	public function __construct(ISecureRandom $secureRandom) {
+		$this->secureRandom = $secureRandom;
+	}
+
+	/**
+	 * generate to token used to authenticate federated shares
+	 *
+	 * @return string
+	 */
+	public function generateToken() {
+		$token = $this->secureRandom->generate(
+			self::TOKEN_LENGTH,
+			ISecureRandom::CHAR_ALPHANUMERIC);
+		return $token;
+	}
+}
