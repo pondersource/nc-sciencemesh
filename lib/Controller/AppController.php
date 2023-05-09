a58ff1591fdbb0f7a1fba6d46693b3cbc101dfa5
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-05-09 17:34:48 +0000

    remove: whitespace

--- lib/Controller/AppController.php
+++ lib/Controller/AppController.php
@@ -134,7 +134,7 @@ class AppController extends Controller {
 
 		return new TextPlainResponse("$meshDirectoryUrl$inviteLinkStr", Http::STATUS_OK);
 	}
-	
+
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
